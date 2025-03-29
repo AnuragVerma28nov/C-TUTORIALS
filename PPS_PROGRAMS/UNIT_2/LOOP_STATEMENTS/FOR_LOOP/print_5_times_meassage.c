949ddf39a8a828ba98ec3855028b8c1405f2b93c
ANURAG VERMA <anuragverma28nov@gmail.com>
2025-03-29 22:00:27 +0530

    Add files via upload

---
+++ PPS_PROGRAMS/UNIT_2/LOOP_STATEMENTS/FOR_LOOP/print_5_times_meassage.c
@@ -0,0 +1,15 @@
+// print 5 times meassage
+
+#include <stdio.h>
+#include<conio.h>
+
+void main()
+{
+    int i;
+    for (i = 0; i < 5; i++)
+    {
+        printf("Hello World\n");
+    }
+    
+    getch();
+}
