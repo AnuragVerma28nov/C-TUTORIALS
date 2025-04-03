2ac30ba9af6ae286c544594bd3fcbd931e1d3867
ANURAG VERMA <anuragverma28nov@gmail.com>
2025-04-03 22:28:41 +0530

    Delete C_STUDY_MATERIALS/CH1 directory

--- C_STUDY_MATERIALS/CH1/1.c
+++
@@ -1,7 +0,0 @@
-// my first code
-# include<stdio.h>
-int  main()
-{
-printf("MY NAME IS ANURAG VERMA");
-return 0;
-} 
--- C_STUDY_MATERIALS/CH1/CH2/9.c
+++
@@ -1,9 +0,0 @@
-//TYPE CONVERSION
-#include<stdio.h>
-#include<math.h>
-int main()
-{
- int a= (int)1.666;
- printf("%d",a);
- return 0;
-}
